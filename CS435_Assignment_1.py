68f3e9f042044951fedd85ccc001c2e7ed83b1e6
Lucas Young <layoung@email.wm.edu>
2024-09-02 12:10:22 -0400

    Added function to draw boxes around gui elements on the images.

--- CS435_Assignment_1.py
+++ CS435_Assignment_1.py
@@ -1,8 +1,11 @@
 import xml.etree.ElementTree as ET
 import os
 
+from PIL import Image
+from PIL import ImageDraw
+
 #function to parse through xml file and find leaf nodes with a bounds attribute
-#returns a list of integers containing information about bounding boxes
+#takes xml file path as parameter and returns a list of integers containing information about bounding boxes
 def parseXML(xml_file):
 
     bounding_boxes = []
@@ -10,7 +13,6 @@ def parseXML(xml_file):
     tree = ET.parse(xml_file)
     root = tree.getroot()
 
-    #iterate over nodes in the tree
     for node in root.iter():
 
         #check if node is a leaf and has bounds attribute
@@ -22,6 +24,24 @@ def parseXML(xml_file):
             bounding_boxes.append(list(map(int, bounds.split(' '))))
 
     return bounding_boxes
+
+#function to draw a rectangle using the Pillow library
+#takes image file path and bounding boxes as parameters and saves modified image as png
+def drawBox(image, bounding_boxes):
+
+    with Image.open(image) as im:
+
+        for bounds in bounding_boxes:
+
+            box = (bounds[0], bounds[1], bounds[2], bounds[3])
+            img1 = ImageDraw.Draw(im)   
+            img1.rectangle(box, outline="yellow", width=5)
+
+
+        try:
+            im.save('test.png')
+        except OSError:
+            print("cannot convert")
 
 
 def main():
@@ -40,7 +60,8 @@ def main():
         else:
             png_list.append(f)
 
-    print(parseXML(folder_path+xml_list[1]))
+    drawBox(folder_path+png_list[1], parseXML(folder_path+xml_list[1]))
+
 
 
 if __name__=="__main__":
